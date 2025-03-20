2895f0877a36df940e78f4951e242a01ecf22cac
tungnhs <tungnhs2014@gmail.com>
2025-03-20 22:26:04 +0700

    Add 05-Thread

---
+++ 05-Thread/Exercise-1/Exercise-1.md
@@ -0,0 +1,59 @@
+# Exercise 1: Basic Thread Creation and Execution
+
+## Description
+Write a C program using the **pthread.h** library to perform the following tasks:
+
+## Instructions
+1. Create two threads. Each thread should print a message along with its ID
+```bash
+Thread 1: Hello from thread.
+```
+2. Use the `pthread_join()` function to wait for both threads to complete before the program exits.
+3. Understand how to use `pthread_create()`and `pthread_join()`, and when a thread terminates.
+
+
+## Hint
+- Use the `pthread_create()` function to create threads and the `pthread_join()`function to wait for threads to finish.
+
+---
+
+## Project Structure
+```
+project/
+├── bin/         
+│   └── exam
+├── Makefile   
+├── obj/         
+│   └── main.o
+└── src/         
+    └── main.c
+```
+---
+
+## Build and Run Instructions
+
+### Build the program
+To build the program, run:
+```bash
+$ make
+```
+
+### Run the program
+To execute the program, use:
+```bash
+$ ./bin/exam
+```
+
+### Example output
+```bash
+Main: Created thread 1
+Thread 1: Hello from thread!
+Main: Created thread 2
+Thread 2: Hello from thread!s
+```
+
+### Clean the project
+To remove all compiled files and binaries, run:
+```bash
+$ make clean
+```
---
+++ 05-Thread/Exercise-1/src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// Thread function prototype
+void* thread_function(void* arg);
+
+int main(int argc, char const *argv[])
+{
+    // Declare an array of thread IDs
+    pthread_t threads[2];
+    int thread_ids[2];
+    int ret;
+
+    // Create two threads
+    for (int i = 0; i < 2; i++) {
+        // Define a unique ID to each thread
+        thread_ids[i] = i + 1;
+
+        // Create a new thread and pass the ID as an argument
+        ret = pthread_create(&threads[i], NULL, thread_function, (void*)&thread_ids[i]);
+        // Check if thread creation was successful
+        if (ret) {
+            printf("Error creating thread %d: %d\n", i + 1, ret);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Main: Created thread %d\n", i + 1);   
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < 2; i++) {
+        ret = pthread_join(threads[i], NULL);
+
+        // Check if joining the thread was successfuls
+        if(ret) {
+            printf("Error joining thread %d: %d\n", i + 1, ret);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
+
+// Thread function to print a message
+void* thread_function(void* arg) {
+    // Get the thread ID from the argument
+    int thread_id = *((int*)arg);
+
+    printf("Thread %d: Hello from thread!\n", thread_id);
+    
+    // Exit the thread
+    pthread_exit(NULL);
+}
